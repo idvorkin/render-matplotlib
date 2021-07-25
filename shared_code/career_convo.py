3f0d8d811313827294a345c73f66d79dbc35f9c7
Igor Dvorkin <idvorkin@gmail.com>
2021-07-25 09:10:13 -0700

    Save career_convo.py

--- shared_code/career_convo.py
+++ shared_code/career_convo.py
@@ -5,6 +5,10 @@ def render(plt):
 
     fig1,  (career_pie, life_pie) = plt.subplots(1, 2)
 
+    # Make the figure bigger to space stuff around
+    height_in_inches = 4
+    fig1.set_size_inches(3 * height_in_inches, height_in_inches)
+
     career_pie.axis(
         "equal"
     )  # Equal aspect ratio ensures that pie is drawn as a circle.
@@ -12,13 +16,13 @@ def render(plt):
     labels = ["Job Title", "Compensation"]
     sizes = [40, 60]
     explode = [0.1, 0]
-    career_pie.pie(sizes, explode=explode, labels=labels, startangle=0)
-    career_pie.set_title("Career Discussions\nDeafult")
+    career_pie.pie(sizes, explode=explode, labels=labels, startangle=70)
+    career_pie.set_title("Normal career\n chat")
     career_pie.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
 
     labels = ["Job Title", "Compensation", "Health", "Hobbies", "Friends and Family"]
     sizes = [20, 15, 10, 10, 10]
     explode = (0.0, 0, 0.1, 0.1, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
-    life_pie.set_title("Career Discussions\nWith the big picture")
-    life_pie.pie(sizes, explode=explode, labels=labels, startangle=300)
+    life_pie.set_title("Big picture career\n chat")
+    life_pie.pie(sizes, explode=explode, labels=labels, startangle=0)
     return plt
